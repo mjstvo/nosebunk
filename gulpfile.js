6a4537f67bbcc13415fbf9e0f0b6934d66c640d3
Matt Stevenson <matthew.stevenson@isarc.co.uk>
2017-02-15 14:01:15 +0000

    theme.css added

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('less', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/freelancer.css')
+    return gulp.src('css/theme.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
