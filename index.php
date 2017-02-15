a924bdc197bcdd7c576caa49d50dd11e5a498f02
Matt Stevenson <matthew.stevenson@isarc.co.uk>
2017-02-15 12:44:20 +0000

    Major changes, rehab

--- index.php
+++ index.php
@@ -1,79 +1,53 @@
-<?php
-/**********************
- * Created by PhpStorm.
- * User: mattstevenson
- * Date: 14/02/2017
- * Time: 8:42 PM
- * Latest Version: 0.0
- * Bug Listings: txt.
- *********************/
-
-include('html/header.html');
-
-?>
-
-<!-- As a link -->
-<nav class="navbar navbar-light bg-faded">
-    <a class="navbar-brand" href="#">
-        <img src="img/mexicana.png" id="mexicana"/>
-    </a>
-</nav>
-
-<div class="container-fluid" id="section1">
-        <div class="col-xs-12">
-            <h1>mexicanajones.com</h1>
-            <div class="col-xs-12">
-            <p>coming soon...</p>
-            </div>
-            <ul class="list-inline">
-                <li>
-                    <a href="http://twitter.com/mexicanajones" id="twitter">
-                        <span class="icon fa fa-twitter" aria-hidden="true">
-                        </span>
-                    </a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
 <style>
-
+    html, body {
+        height: 100%;
+        margin: 0;
+    }
+    p{
+        text-align:center;
+        font-family: 'Helvetica Neue', cursive;
+        font-size: 1.5em;
+        text-shadow: -2px 3px 0 #ff00ff, 4px 1px 0 #00ffff, -4px -3px 0 #ffff00;
+        float:right;
+    }
     h1 {
+        text-align:center;
         font-family: 'Helvetica Neue', cursive;
         font-size: 3em;
         line-height: 1;
         margin: 20px;
-    }
-
-    a {
-        color: #1a1a1a;
-    @include transition(all 0.3s ease-in-out);
-        text-decoration: none;
-        position: relative;
         text-shadow: -2px 3px 0 #ff00ff, 4px 1px 0 #00ffff, -4px -3px 0 #ffff00;
-
-
-    &:hover {
-     @include animation(link-hover 1s infinite);
-     }
+        transition: 1s;
     }
 
-    section {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-    @include transform(translate(-50%, -50%));
+    h1:hover{
+        transform: scale(1.1, 1.1);
+        -webkit-transform: scale(1.1,1.1);
 
-    &:hover {
-     @include animation(pulse 0.3s infinite);
-     }
     }
 
-    h1 {
+    #twitter {
+    @include transition(all 0.3 s ease-in-out);
         text-shadow: -2px 3px 0 #ff00ff, 4px 1px 0 #00ffff, -4px -3px 0 #ffff00;
+        float:left;
+        display: block;
+    }
+
+
+    #main {
+        height: 90%;
+        width:100%;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #mexicana{
+        height:3em;
+        width:3em;
     }
 </style>
+
+<div class="container" id="main">
+    <h1>mexicanajones.com</h1>
+</div>
